28aeb462677cc910a8257a399e25f367d4170868
marvinjameserosa <marvinjameserosa@gmail.com>
2025-07-25 05:13:55 +0800

    fix: type conversion for map

--- frontend/src/app/components/LeafletMap.tsx
+++ frontend/src/app/components/LeafletMap.tsx
@@ -14,7 +14,8 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
 // Fix default marker icons
-delete (L.Icon.Default.prototype as Record<string, unknown>)._getIconUrl;
+delete (L.Icon.Default.prototype as unknown as Record<string, unknown>)
+  ._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
